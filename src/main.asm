cefa316ae5bf64a34f00737ee5a27c457dff1e8a
Quint Guvernator <quint@guvernator.net>
2019-03-03 17:56:25 +0100

    tapering-off sin wave

--- src/main.asm
+++ src/main.asm
@@ -10,7 +10,6 @@ section "entry",ROM0[$100]
 
 section "main",ROM0[$0150]
 
-
 ; a = nybble in question
 bring_closer_to_eight:
     cp $08
@@ -22,34 +21,6 @@ bring_closer_to_eight:
 .raise
     add 1
 .end
-    ret
-
-decrease_wave_volume:
-    push hl                             ; hl = hiram address
-    push bc                             ; b = processed low nybble, c = sample value
-    ld h,$ff
-    ld l,$80
-.next_byte
-    ld a,[hl]                           ; get a pair of samples
-    ld c,a                              ; save it to c and a
-    and $0f                             ; work on the lower nybble first
-    call bring_closer_to_eight
-    ld b,a
-    ld a,c                              ; now the upper nybble
-    rept 4
-    srl a
-    endr
-    call bring_closer_to_eight
-    rept 4
-    sla a
-    endr
-    or b                                ; combine the two nybbles again
-    ld [hl+],a                          ; save to hiram
-    ld a,l
-    cp $90
-    jr nz,.next_byte
-    pop bc
-    pop hl
     ret
 
 start
@@ -73,22 +44,35 @@ ANGLE = ANGLE + 4096.0                  ; circle has 65536.0 degrees, sin range 
 ; global audio settings
     ld a,$80                            ; turn audio on globally
     ld [rAUDENA],a
-    ld a,$44                            ; WAV channel output on
+    ldz                                 ; WAV channel output OFF (needed?)
     ld [rAUDTERM],a
     ld a,$77                            ; master volume max
     ld [rAUDVOL],a
 
-    ld l,$40                            ; l is a counter to dec samples
-    ld h,a                              ; h is unused here
-    push hl
+decrease_volume                         ; cycle through wave samples in hiram buffer and decrease volume
+    ld h,$ff                            ; hl = hiram address
+    ld l,$80
+.next_byte
+    ld a,[hl]                           ; get a pair of samples
+    ld c,a                              ; save original sample -> c for later
+    and $0f                             ; extract the lower nybble
+    call bring_closer_to_eight          ; process it to make it quieter
+    ld b,a                              ; save processed low nybble -> b
+    ld a,c                              ; now extract upper nybble
+    rept 4
+    srl a
+    endr
+    call bring_closer_to_eight          ; process it to make it quieter
+    rept 4                              ; recombine the two nybbles
+    sla a
+    endr
+    or b
+    ld [hl+],a                          ; save new result to hiram
+    ld a,l                              ; out of samples?
+    cp $90
+    jr nz,.next_byte
 
-copy_wave
-    dec l                               ; ++l, do we need to decrease volume?
-    jr nz,begin_wait_loop
-    call z,decrease_wave_volume
-    ld l,$10
-
-    ldz
+    ldz                                 ; about to write to buffer, turn off WAV sound
     ld [rAUDTERM],a                     ; WAV channel output off
     ld [rAUD3ENA],a                     ; WAV channel off
 
@@ -119,15 +103,15 @@ copy_wave
     ld a,(HIGH(c4_freq) & $07) | $80    ; play C2 (high byte) + some flags + trigger
     ld [rAUD3HIGH],a
 
-begin_wait_loop:
-    ld b,10                             ; delay timers
+wait_loop:
     ldz
-
-wait_loop:                               ; busy loop
-    inc a
-    jr nz,wait_loop
+    ld b,$48
+.loop
+    nop
+    dec a
+    jr nz,.loop
     dec b
-    jr nz,wait_loop
-    jr copy_wave
+    jr nz,.loop
+    jr decrease_volume
 
 ; vim: se ft=rgbds et ts=4 sts=4 sw=4:
